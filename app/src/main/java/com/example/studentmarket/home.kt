47386e1bcdd2988beb0d3060d51635164f557d48
shgnplaatjies <plaatjiesshagan@gmail.com>
2021-08-06 13:38:35 +0200

    Created Product List Skeleton and removed previous RecyclerView Code

--- app/src/main/java/com/example/studentmarket/home.kt
+++ app/src/main/java/com/example/studentmarket/home.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_home.*
+import com.example.studentmarket.adapters.CardAdapter
+import java.util.*
 
 
 /**
@@ -18,6 +21,7 @@ class home : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
     }
 
     override fun onCreateView(
@@ -28,6 +32,23 @@ class home : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    /** Shagan
+     * Get Product List and return list of product objects
+     * Param: None so far, can update to include product filter as param...
+     * Returns: ArrayList of products objects
+     */
+    private fun getProductList(): ArrayList<String>{
+        val productList = ArrayList<String>()
+        val itemCount = 15 // Find size of product data
+        for (i in 1..itemCount){
+            productList.add("This is product number$i")
 
+            /** Shagan for Nicolle (?)
+             * The code for adding the product objects
+             * Please initialize and add from the dB
+             */
+        }
+        return productList
+    }
 
 }
