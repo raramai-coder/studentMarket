3fa40047962cecad6330369b89af8758a5384602
raramai-coder <1846409@students.wits.ac.za>
2021-08-08 16:11:30 +0200

    the app will deploy now on god i swearlod

--- studentMarketApi/settings.py
+++ studentMarketApi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,3 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import django_on_heroku
 django_on_heroku.settings(locals())
+
+django_heroku.settings(locals())
