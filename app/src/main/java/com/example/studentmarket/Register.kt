ccf0212c334b766360db3e5c34aef0ad7db633a2
shgnplaatjies <plaatjiesshagan@gmail.com>
2021-08-08 14:49:57 +0200

    Added privacy policy link to the Registration page

--- app/src/main/java/com/example/studentmarket/Register.kt
+++ app/src/main/java/com/example/studentmarket/Register.kt
@@ -1,15 +1,20 @@
 package com.example.studentmarket
 
+import android.graphics.Color
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.text.method.LinkMovementMethod
+import kotlinx.android.synthetic.main.fragment_privacy_popup.*
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        setUpHyperlink()
         //region Register Button
         val register_btn = findViewById<Button>(R.id.register_btn_rg)
 
@@ -18,6 +23,15 @@ class Register : AppCompatActivity() {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
-        //endregion
+    }
+
+    fun setUpHyperlink() {
+        val linkPrivacy = findViewById<TextView>(R.id.textview_terms_conditions)
+        val linkTermsConditions = findViewById<TextView>(R.id.textview_privacy_policy)
+//        val linkTextView = findViewByID<TextView>(R.id.fragment_privacy_popup)
+        linkPrivacy.setMovementMethod(LinkMovementMethod.getInstance())
+        linkTermsConditions.setMovementMethod(LinkMovementMethod.getInstance())
+        linkPrivacy.setLinkTextColor(Color.BLUE)
+        linkTermsConditions.setLinkTextColor(Color.BLUE)
     }
 }
